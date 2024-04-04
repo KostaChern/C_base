73cbd0e926b1452e24a467134852809afa07d6d7
Konstantin_Chern <codahl@yandex.ru>
2024-04-04 13:18:43 +0300

    new file:   Task_1.c

--- Seminars/Seminar_2.c
+++ Seminars/Seminar_2.c
@@ -75,10 +75,26 @@ int main(void){
 	// float s = (a + n + d) / 3.f;
 	// printf("%.2f\n", s);
 
-	int x;
-	scanf("%d", &x);
-	printf("%d", (x / 100) * ((x % 100) / 10) * (x % 10));
-
-
-	return 0;
+	// int x;
+	// scanf("%d", &x);
+	// printf("%d", (x / 100) * ((x % 100) / 10) * (x % 10));
+int prime(int n)
+{
+ int i=2;
+ while (i*i<=n){
+ if (n%i==0)
+ return 0;
+ i++;
+ }
+ return 1;
+}
+int main()
+{
+ int num;
+ scanf("%d",&num);
+ prime(num) ? printf("Prime") : printf("Not prime");
+//printf("%s",prime(num) ? "Prime" : "Not prime");
+//if (prime(num)) printf("Prime"); else printf("Not prime");
+ return 0;
+}
 }
