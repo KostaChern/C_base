c1aba2bec9733757115f8ecb7328e8903ef3d0e5
Konstantin_Chern <codahl@yandex.ru>
2024-04-18 18:18:05 +0300

    new file:   HW_8/Task_3.c

--- HW_8/Task_2.c
+++ HW_8/Task_2.c
@@ -28,10 +28,7 @@ void PrintArray(int arr[], int n)
 
 int main()
 {
-    enum
-    {
-        BUFFER_SIZE = 20
-    };
+    enum { BUFFER_SIZE = 20 };
     int ar[] = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     sort_even_odd(BUFFER_SIZE, ar);
     PrintArray(ar, BUFFER_SIZE);
