316acfd7e7e46f473dec93e8a0467c4cecd7a847
Konstantin_Chern <codahl@yandex.ru>
2024-04-30 18:04:08 +0300

    modified:   CourseWorkOnTheC/CourseWork/main.c

--- CourseWorkOnTheC/CourseWork/main.c
+++ CourseWorkOnTheC/CourseWork/main.c
@@ -1,60 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include "temp_functions.h"
-
-int main(int argc, char *argv[])
+#include <stdio.h> 
+#include <conio.h> 
+#define N 6
+int main(int argc, char **argv)
 {
-    char * file_name;       //Переменная для параметра -f
-    int input_month = 0;    //Переменная для параметра -m
-    int rez_arg = 0;        //Переменная для аргументов командной строки
-    opterr=0;
-    while ((rez_arg = getopt(argc, argv, "hf:m::")) != -1)
+    FILE *fp;
+    char fileName[] = "temperature_small.csv";
+    fp = fopen(fileName, "r");
+    if(fp == NULL)
+        return 1;
+    int Y, M, D, H, Min, T;
+    int r;
+    while((r = fscanf(fp,"%d;%d;%d;%d;%d;%d",&Y, &M, &D, &H, &Min, &Y))>0)
     {
-        switch (rez_arg)
+        if(r<N)
         {
-        case 'h':
-            help();
-            break;
-        case 'f':
-            file_name = optarg;
-            break;
-        case 'm':
-            input_month = atoi(optarg);
-            break;
-        case '?':
-            printf("Error : incorrect parameter!\n");
-            break;
-        };
-    };
-
-    if (argc < 2){
-        printf("\nThe program reads the temperature sensor data from a file\n");
-        printf("and prints statistics for the year or month\n");
-        printf("To output instructions, enter the parameter > -h <\n");
-    }
-
-    if(file_name && ((input_month >= 0) && (input_month < 13)))
-    {
-        printf("%s\n", file_name);
-        struct sensor * info;                                               //Объявление массива структурного типа
-        info = (struct sensor *)malloc(SIZE * sizeof(struct sensor));       
-        long number_of_lines = add_info(info, file_name);                   //Основной счетчик количества элементов в массиве
-        if(input_month == 0)
-        {
-            statistics_output(info, number_of_lines);
+            char s[20];
+            r = fscanf(fp, "%[^\n]", s);
+            printf("ERROR %d=%s\n",r,s);
         }
-        else 
-        {
-            statistics_output_month(info, number_of_lines, input_month);
-        }
-        free(info);
-    }
-    else
-    {
-        printf("\nIncorrect parameter\n");
-        help();
+        else
+            printf("%d;%d;%d;%d;%d;%d\n", Y, M, D, H, Min, T); 
     }
     return 0;
 }
